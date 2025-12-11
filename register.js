aab959650f0337b26620a08adc439a220993e70b
Vanshi15605 <vanshikasharma15605@gmail.com>
2025-12-11 17:04:02 +0000

    This is first commit

---
+++ register.js
@@ -0,0 +1,7 @@
+localStorage.setItem("voyagoUser", JSON.stringify({
+    phone: contact.value.trim(),
+    email: contact.value.trim(),
+    fullname: fullname.value.trim(),
+    username: username.value.trim(),
+    password: password.value.trim()
+}));
